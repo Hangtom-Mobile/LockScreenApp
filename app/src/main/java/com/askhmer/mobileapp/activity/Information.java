bf827105c476a0fe3eb099c91899b35434dccc83
Soklundy <bonglundy208@gmail.com>
2016-06-17 22:26:44 -0700

    toolbar center

--- app/src/main/java/com/askhmer/mobileapp/activity/Information.java
+++ app/src/main/java/com/askhmer/mobileapp/activity/Information.java
@@ -14,6 +14,7 @@ public class Information extends AppCompatActivity {
         setContentView(R.layout.activity_information);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
 
     }
