76729bd62bed7500c22f8bb6705f5eaa188a91eb
Longdy <longdygold@gmail.com>
2016-06-22 09:14:17 +0700

    edit

--- app/src/main/java/com/askhmer/mobileapp/activity/Age.java
+++ app/src/main/java/com/askhmer/mobileapp/activity/Age.java
@@ -1,27 +1,31 @@
 package com.askhmer.mobileapp.activity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.askhmer.mobileapp.R;
+import com.askhmer.mobileapp.utils.MyDatePickerDialog;
 import com.askhmer.mobileapp.utils.SharedPreferencesFile;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 
-public class Age extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class Age extends AppCompatActivity{
 
-    private Spinner year;
     private SharedPreferencesFile mSharedPrefrencesFile;
+    private EditText etDOB;
+    private int year, month, day;
+    private Calendar calendar;
+    private String sDOB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,91 +37,81 @@ public class Age extends AppCompatActivity implements AdapterView.OnItemSelected
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         mSharedPrefrencesFile = SharedPreferencesFile.newInstance(getApplicationContext(),SharedPreferencesFile.FILE_INFORMATION_TEMP);
-        year = (Spinner) findViewById(R.id.sp_year);
-        year.setPrompt("Select year you was born!!!");
 
-        ArrayList<Integer> y = new ArrayList<>();
-        for(int i = 2016; i >= 1930; i--){
-            y.add(i);
-        }
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
 
-        // Creating adapter for spinner
-        ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, y);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        etDOB = (EditText) findViewById(R.id.et_age);
+        etDOB.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
 
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        year.setAdapter(dataAdapter);
-
-        final Button button = (Button)findViewById(R.id.bttn_next);
-        button.setOnClickListener(new View.OnClickListener() {
+                if(hasFocus){
+                    setDate(v);
+                }else {
+                    //Toast.makeText(getApplicationContext(), "lost the focus", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        etDOB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calendar now = Calendar.getInstance();
-                Intent intent = new Intent(getApplicationContext(), Location.class);
-
-                int currentyear = now.get(Calendar.YEAR);
-                int userYear = Integer.parseInt(year.getSelectedItem().toString());
-                int resultYear = currentyear - userYear;
-
-                Log.d("yearAge", resultYear + "");
-                mSharedPrefrencesFile.putStringSharedPreference(SharedPreferencesFile.KEY_INFORMATION_TEMP_AGE, resultYear+"");
-                startActivity(intent);
+                setDate(v);
             }
         });
 
+        final Button button = (Button)findViewById(R.id.bttn_next);
+               button.setOnClickListener(new View.OnClickListener() {
+                       @Override
+                       public void onClick(View v) {
+                           Toast.makeText(Age.this, ""+sDOB, Toast.LENGTH_SHORT).show();
+                               Intent intent = new Intent(getApplicationContext(), Location.class);
+                               mSharedPrefrencesFile.putStringSharedPreference(SharedPreferencesFile.KEY_INFORMATION_TEMP_AGE, sDOB);
+                               startActivity(intent);
+                           }
+                   });
 
-/*
-        etYear.setOnClickListener(new View.OnClickListener() {
-            // @Override
-            public void onClick(View v) {
-                final Calendar c = Calendar.getInstance();
+    }
 
-                int y = c.get(Calendar.YEAR) -18;
-                int m = 0;
-                int d = 0;
-                final String[] MONTH = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-
-                DatePickerDialog dp = new DatePickerDialog(Age.this,
-                        new DatePickerDialog.OnDateSetListener() {
-
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
-                                String erg = "";
-                                erg = String.valueOf(dayOfMonth);
-                                erg += "." + String.valueOf(monthOfYear + 1);
-                                erg += "." + year;
-
-                                ((TextView) etYear).setText(erg);
-
-                            }
-
-                        }, y, m, d);
-                dp.setTitle("Calender");
-                dp.setMessage("Select Your Graduation date Please?");
-
-                dp.show();
-
-
-            }
-        });
-*/
-
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
-
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-    }
-    public void onNothingSelected(AdapterView<?> arg0) {
+    protected Dialog onCreateDialog(int id) {
         // TODO Auto-generated method stub
+        if (id == 999) {
+            MyDatePickerDialog mDatePicker = new MyDatePickerDialog(this,myDateListener,year -18, month, day);
+            mDatePicker.setPermanentTitle("Your date of birth");
+//            return new DatePickerDialog(this, myDateListener, year, month, day);
+            return mDatePicker;
+        }
+        return null;
     }
 
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+            showDate(arg1, arg2 + 1, arg3);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        String sMonth = month+"";
+        String sDay = day+"";
+        if(sMonth.length()== 1){
+            sMonth = "0"+sMonth;
+        }
+        if(sDay.length()== 1){
+            sDay = "0"+sDay;
+        }
+        sDOB = new StringBuilder().append(year).append(sMonth).append(sDay)+"";
+        etDOB.setText("" + new StringBuilder().append(year).append("/").append(sMonth).append("/").append(sDay), TextView.BufferType.NORMAL);
+    }
+    
 }
 
--- app/src/main/java/com/askhmer/mobileapp/activity/PhoneNumber.java
+++ app/src/main/java/com/askhmer/mobileapp/activity/PhoneNumber.java
@@ -4,16 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.askhmer.mobileapp.R;
 import com.askhmer.mobileapp.utils.SharedPreferencesFile;
 
+import java.util.regex.Pattern;
+
 public class PhoneNumber extends AppCompatActivity {
 
     private SharedPreferencesFile mSharedPrefrencesFile;
+    private EditText etPhoneNum;
+    private TextView tvMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +33,64 @@ public class PhoneNumber extends AppCompatActivity {
 
         mSharedPrefrencesFile = SharedPreferencesFile.newInstance(getApplicationContext(),SharedPreferencesFile.FILE_INFORMATION_TEMP);
 
-        final EditText editText = (EditText)findViewById(R.id.et_phone_number);
+        etPhoneNum = (EditText)findViewById(R.id.et_phone_number);
+        tvMsg = (TextView) findViewById(R.id.ve_phone_num);
         Button button = (Button)findViewById(R.id.bttn_next);
 
+        etPhoneNum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(isValidMobile(etPhoneNum.getText().toString())){
+                    tvMsg.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), Information.class);
-                mSharedPrefrencesFile.putStringSharedPreference(SharedPreferencesFile.KEY_INFORMATION_TEMP_PHONE, editText.getText().toString());
-                startActivity(i);
+                if(isValidMobile(etPhoneNum.getText().toString())){
+                    Intent i = new Intent(getApplicationContext(), Information.class);
+                    mSharedPrefrencesFile.putStringSharedPreference(SharedPreferencesFile.KEY_INFORMATION_TEMP_PHONE, etPhoneNum.getText().toString());
+                    startActivity(i);
+                }else {
+                    tvMsg.setVisibility(View.VISIBLE);
+                }
+                
             }
         });
+    }
+
+    private boolean isValidMobile(String phone)
+    {
+        boolean check=false;
+        if(!Pattern.matches("[a-zA-Z]+", etPhoneNum.getText().toString()))
+        {
+            if(phone.length() < 9 || phone.length() > 10)
+            {
+                check = false;
+            }
+            else
+            {
+                check = true;
+            }
+        }
+        else
+        {
+            check=false;
+        }
+        return check;
+//        return android.util.Patterns.PHONE.matcher(phone).matches();
     }
 }
