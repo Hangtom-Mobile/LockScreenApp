88f3e93608702b38624a5c5ec68f0be67c20ad58
Soklundy <bonglundy208@gmail.com>
2016-06-28 11:15:40 +0700

    myinfo

--- app/src/main/java/com/askhmer/mobileapp/activity/MyInfo.java
+++ app/src/main/java/com/askhmer/mobileapp/activity/MyInfo.java
@@ -2,16 +2,40 @@ package com.askhmer.mobileapp.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.askhmer.mobileapp.R;
+import com.askhmer.mobileapp.network.API;
+import com.askhmer.mobileapp.network.MySingleton;
+import com.askhmer.mobileapp.utils.SharedPreferencesFile;
 import com.liuguangqiang.swipeback.SwipeBackActivity;
 import com.liuguangqiang.swipeback.SwipeBackLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyInfo extends SwipeBackActivity {
 
     private Spinner location;
+    private SharedPreferencesFile mSharedPreferencesFile;
+    private TextView edName, edId, edPhone, edDob, edSex;
+    private Button btnOk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +47,13 @@ public class MyInfo extends SwipeBackActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
+        mSharedPreferencesFile = SharedPreferencesFile.newInstance(getApplication(),SharedPreferencesFile.FILE_INFORMATION_TEMP);
+        edName = (TextView) findViewById(R.id.et_name);
+        edId = (TextView) findViewById(R.id.et_id);
+        edPhone = (TextView) findViewById(R.id.et_phone);
+        edDob = (TextView) findViewById(R.id.et_dob);
+        edSex = (TextView) findViewById(R.id.et_sex);
+        btnOk = (Button) findViewById(R.id.btn_ok);
 
         location = (Spinner) findViewById(R.id.sp_location);
         location.setPrompt("Select year location!!!");
@@ -37,7 +67,64 @@ public class MyInfo extends SwipeBackActivity {
         // attaching data adapter to spinner
         location.setAdapter(dataAdapter);
 
+        /*request to server*/
+        requesetMemberInfo();
 
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+    }
+
+    public void requesetMemberInfo() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, API.MEMBERINFOAPI,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("myInfo", response);
+                        if (!response.isEmpty()) {
+                            try {
+                                JSONObject jsonObj = new JSONObject(response);
+                                if (jsonObj.getString("rst").equals("110")) {
+                                    edName.setText(jsonObj.getString("mb_name"));
+                                    edId.setText(mSharedPreferencesFile.getStringSharedPreference(SharedPreferencesFile.KEY_INFORMATION_TEMP_CASHID));
+                                    edPhone.setText(jsonObj.getString("mb_hp"));
+                                    edDob.setText(jsonObj.getString("mb_birth"));
+                                    location.setSelection(Integer.parseInt(jsonObj.getString("mb_location"))-1);
+                                    edSex.setText(jsonObj.getString("mb_sex"));
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ErroVelloy MyInfo", error.toString());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("cash_slide_id", mSharedPreferencesFile.getStringSharedPreference(SharedPreferencesFile.KEY_INFORMATION_TEMP_CASHID));
+                return params;
+            }
+            @Override
+            public void deliverError(VolleyError error) {
+                if (error instanceof NoConnectionError) {
+                    Cache.Entry entry = this.getCacheEntry();
+                    if(entry != null) {
+                        Response<String> response = parseNetworkResponse(new NetworkResponse(entry.data, entry.responseHeaders));
+                        deliverResponse(response.result);
+                        return;
+                    }
+                }
+                super.deliverError(error);
+            }
+        };
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
     }
 }
