067b6e8acc97d5b266a1c67a1c9432b9b3ec4e43
Soklundy <bonglundy208@gmail.com>
2016-08-03 15:50:56 +0700

    call

--- app/src/main/java/com/askhmer/mobileapp/activity/LockScreenActivity.java
+++ app/src/main/java/com/askhmer/mobileapp/activity/LockScreenActivity.java
@@ -166,12 +166,10 @@ public class LockScreenActivity extends Activity implements
 	private class StateListener extends PhoneStateListener {
 		@Override
 		public void onCallStateChanged(int state, String incomingNumber) {
-
-			super.onCallStateChanged(state, incomingNumber);
+			/*super.onCallStateChanged(state, incomingNumber);*/
 			switch (state) {
 			case TelephonyManager.CALL_STATE_RINGING:
 				unlockHomeButton();
-				wakeUp();
 				break;
 			case TelephonyManager.CALL_STATE_OFFHOOK:
 				break;
--- app/src/main/java/com/askhmer/mobileapp/adapter/FullScreenImageAdapter.java
+++ app/src/main/java/com/askhmer/mobileapp/adapter/FullScreenImageAdapter.java
@@ -56,7 +56,7 @@ public class FullScreenImageAdapter extends PagerAdapter {
         txtBasicPoint = (TextView) viewLayout.findViewById(R.id.txt_basic_point);
         txtViewPoint = (TextView) viewLayout.findViewById(R.id.txt_view_point);
 
-        Picasso.with(container.getContext()).load(_imagePaths.get(position).getImageUrl()).fit().placeholder(R.drawable.no_inte_slow).into(imgDisplay);
+        Picasso.with(container.getContext()).load(_imagePaths.get(position).getImageUrl()).placeholder(R.drawable.no_inte_slow).into(imgDisplay);
 
         txtBasicPoint.setText("+ " + _imagePaths.get(position).getLockBasicPrice());
         txtViewPoint.setText("+ " + _imagePaths.get(position).getLockViewPrice());
