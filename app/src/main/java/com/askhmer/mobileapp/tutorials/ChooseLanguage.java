8961970c8c7c634c6e76693a29ce92335b9815b4
Soklundy <bonglundy208@gmail.com>
2016-08-15 11:31:41 +0700

    l

--- app/src/main/java/com/askhmer/mobileapp/tutorials/ChooseLanguage.java
+++ app/src/main/java/com/askhmer/mobileapp/tutorials/ChooseLanguage.java
@@ -4,12 +4,16 @@ package com.askhmer.mobileapp.tutorials;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.askhmer.mobileapp.R;
+import com.askhmer.mobileapp.activity.TermsOfUse;
 import com.askhmer.mobileapp.utils.MutiLanguage;
 
 /**
@@ -18,19 +22,23 @@ import com.askhmer.mobileapp.utils.MutiLanguage;
 public class ChooseLanguage extends AppCompatActivity {
 
     private MutiLanguage mutiLanguage;
+    private RadioButton radioBtnEn;
+    private RadioButton radioBtnKm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_langauge);
+        setContentView(R.layout.activity_choose_lan);
 
-        final RadioGroup toggle = (RadioGroup) findViewById(R.id.radio_language);
-        final int selectedId = toggle.getCheckedRadioButtonId();
-        RadioButton radioBtnEn = (RadioButton) findViewById(R.id.radio_english);
-        RadioButton radioBtnKm = (RadioButton) findViewById(R.id.radio_khmer);
-        Button buttonStart = (Button) findViewById(R.id.bttn_start);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-         mutiLanguage = new MutiLanguage(getApplicationContext(), this);
+        radioBtnEn = (RadioButton) findViewById(R.id.radio_english);
+        radioBtnKm = (RadioButton) findViewById(R.id.radio_khmer);
+        final Button buttonStart = (Button) findViewById(R.id.bttn_start);
+
+        mutiLanguage = new MutiLanguage(getApplicationContext(), this);
         String lang = mutiLanguage.getLanguageCurrent();
 
         if (lang.equals("en") || lang.isEmpty()) {
@@ -42,15 +50,37 @@ public class ChooseLanguage extends AppCompatActivity {
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RadioGroup toggle = (RadioGroup) findViewById(R.id.radio_language);
-                if (toggle.getCheckedRadioButtonId() == R.id.radio_khmer) {
+                if (radioBtnKm.isChecked() == true) {
                     mutiLanguage.setLanguage("km");
-                } else {
+                } else if (radioBtnEn.isChecked() == true) {
                     mutiLanguage.setLanguage("en");
                 }
 
                 Intent intent = new Intent(getApplication(), MainPage.class);
                 startActivity(intent);
+            }
+        });
+
+         /*select only one radio at one time lisener*/
+        selectOnlyOneRadioAtOneTime();
+    }
+
+    private void selectOnlyOneRadioAtOneTime() {
+        radioBtnKm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true) {
+                    radioBtnEn.setChecked(false);
+                }
+            }
+        });
+
+        radioBtnEn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true) {
+                    radioBtnKm.setChecked(false);
+                }
             }
         });
     }
