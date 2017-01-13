bf270a9a67002b6af724b2de886489d6c0900e73
soklundy <bonglundy208@gmail.com>
2017-01-13 17:31:51 +0700

    video

--- app/src/main/java/com/askhmer/lockscreen/adapter/AdpterNewFeed.java
+++ app/src/main/java/com/askhmer/lockscreen/adapter/AdpterNewFeed.java
@@ -1,5 +1,12 @@
 package com.askhmer.lockscreen.adapter;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,9 +14,18 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.askhmer.lockscreen.R;
 import com.askhmer.lockscreen.model.VideoNewFeed;
+import com.askhmer.lockscreen.utils.Config;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+import com.google.android.youtube.player.YouTubeThumbnailLoader;
+import com.google.android.youtube.player.YouTubeThumbnailView;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -18,11 +34,12 @@ import java.util.List;
  * Created by medayi01 on 1/12/2017.
  */
 
-public class AdpterNewFeed extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+public class AdpterNewFeed extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<VideoNewFeed> videoNewFeeds;
     private static final int TYPE_ITEM = 0;
     private static final int TYPE_LOADING = 1;
+    private Context context;
 
     public class MyViewHolderloading extends RecyclerView.ViewHolder {
 
@@ -34,20 +51,37 @@ public class AdpterNewFeed extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         }
     }
 
-    public class MyViewHolderItem extends RecyclerView.ViewHolder {
+    public class MyViewHolderItem extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public ImageView imageView;
         public TextView title;
+        public ImageView btnPlay;
+        public YouTubeThumbnailView youTubeThumbnailView;
 
         public MyViewHolderItem(View view) {
             super(view);
-            imageView = (ImageView) view.findViewById(R.id.image_thumnail);
             title = (TextView) view.findViewById(R.id.txt_des);
+            btnPlay = (ImageView) view.findViewById(R.id.play_btn);
+            youTubeThumbnailView = (YouTubeThumbnailView) view.findViewById(R.id.youtube_thumbnail);
+
+            btnPlay.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            String position = v.getTag().toString();
+            Intent intent = YouTubeStandalonePlayer.createVideoIntent((Activity) context,
+                    Config.DEVELOPER_KEY,
+                    position,//video id
+                    100,     //after this time, video will start automatically
+                    true,               //autoplay or not
+                    false);             //lightbox mode or not; show the video in a small box
+            context.startActivity(intent);
         }
     }
 
-    public AdpterNewFeed(List<VideoNewFeed> videoNewFeeds) {
+    public AdpterNewFeed(List<VideoNewFeed> videoNewFeeds, Context context) {
         this.videoNewFeeds = videoNewFeeds;
+        this.context = context;
     }
 
     @Override
@@ -68,10 +102,36 @@ public class AdpterNewFeed extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         if(holder instanceof MyViewHolderItem) {
             MyViewHolderItem myViewHolderItem = (MyViewHolderItem)holder;
-            VideoNewFeed videoNewFeed = getItem(position);
+            final VideoNewFeed videoNewFeed = getItem(position);
 
             myViewHolderItem.title.setText(videoNewFeed.getTitle());
-            Picasso.with(myViewHolderItem.imageView.getContext()).load(videoNewFeed.getThumnail()).into(myViewHolderItem.imageView);
+            myViewHolderItem.btnPlay.setTag(videoNewFeed.getVideoId());
+
+            final YouTubeThumbnailLoader.OnThumbnailLoadedListener  onThumbnailLoadedListener = new YouTubeThumbnailLoader.OnThumbnailLoadedListener(){
+                @Override
+                public void onThumbnailError(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader.ErrorReason errorReason) {
+
+                }
+
+                @Override
+                public void onThumbnailLoaded(YouTubeThumbnailView youTubeThumbnailView, String s) {
+                    youTubeThumbnailView.setVisibility(View.VISIBLE);
+                }
+            };
+
+
+            myViewHolderItem.youTubeThumbnailView.initialize(Config.DEVELOPER_KEY, new YouTubeThumbnailView.OnInitializedListener() {
+                @Override
+                public void onInitializationSuccess(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader youTubeThumbnailLoader) {
+                    youTubeThumbnailLoader.setVideo(videoNewFeed.getVideoId());
+                    youTubeThumbnailLoader.setOnThumbnailLoadedListener(onThumbnailLoadedListener);
+                }
+
+                @Override
+                public void onInitializationFailure(YouTubeThumbnailView youTubeThumbnailView, YouTubeInitializationResult youTubeInitializationResult) {
+
+                }
+            });
         }
         else if(holder instanceof MyViewHolderloading) {
             MyViewHolderloading myViewHolderloading = (MyViewHolderloading) holder;
@@ -91,10 +151,7 @@ public class AdpterNewFeed extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     @Override
     public int getItemViewType(int position) {
-        if (isPositionHeader(position)) {
-            return TYPE_LOADING;
-        }
-        return TYPE_ITEM;
+        return videoNewFeeds.get(position) == null ? TYPE_LOADING : TYPE_ITEM;
     }
 
     private boolean isPositionHeader(int position) {
